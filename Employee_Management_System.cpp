dd501cfe0c1ff47f1a3f437301e842ddb2077d4d
Ro-h-an <rohankasliwal15@gmail.com>
2024-08-03 21:08:37 +0530

    Employee Details Code Done

--- Employee_Management_System.cpp
+++ Employee_Management_System.cpp
@@ -1,6 +1,47 @@
 #include<iostream>
+#include<conio.h>
+#include<fstream>
+#include<stdlib.h>
+#include<iomanip>
+#include<string.h>
+#include<stdio.h>
+const char* fileName="Employee.txt";
 using namespace std;
 
-int main(){
-    return 0;
+class Employee{
+    private:
+    int EmpID;
+    char EmpName[50],Post[50],Department[10];
+    float Salary;
+    public:
+    void ReadData();
+    int GetID();
+    void DisplayRecord();
+    char* GetDepartment();
+};
+
+void Employee::ReadData(){
+    cout<<endl<<"Employee ID:";
+    cin>>EmpID;
+    cout<<"Employee Name:";
+    cin>>EmpName;
+    cout<<"Employee's Post:";
+    cin>>Post;
+    cout<<"Employee's Department:";
+    cin>>Department;
+    cout<<"Salary:";
+    cin>>Salary;
+}
+
+void Employee::DisplayRecord(){
+    cout<<endl<<"_______________________________";
+    cout<<endl<<setw(5)<<EmpID<<setw(15)<<EmpName<<setw(15)<<Post<<setw(15)<<Department<<setw(8)<<Salary;
+}
+
+int Employee::GetID(){
+    return EmpID;
+}
+
+char* Employee::GetDepartment(){
+    return Department;
 }
